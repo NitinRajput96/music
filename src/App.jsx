c33130a7ef74ade51ec6f1f07766cb56f2766012
nitinrajput96 <nitin965432@gmail.com>
2024-09-14 16:17:14 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect,useState} from 'react'
 import './App.css'
+import { FaPlay } from "react-icons/fa";
+import { FaPause } from "react-icons/fa";
 
 function App() {
         
   const audio=document.getElementById("musicplay")
+  const [state,setState]=useState(false)
   
-  useEffect(()=>{
-   
-  },[])
   
-
   const audPaly=()=>{
-       audio.play()
+       audio.play()     
   }
 
   const audioPush=()=>{
       audio.pause()
   }
+
+  useEffect(()=>{
+             
+  },[])
               
    
   return (
     <>
-       <div className=' w-52 h-52  bg-slate-700'>
-       <audio id='musicplay' >
+        <audio id='musicplay' >
            <source src="/music/old_Aakrosh - Saude Bazi (Encore).mp3" />
         </audio>
-        <button onClick={audPaly}>Play</button>
-        <button onClick={audioPush}>Poush</button>
-      
+
+       <div className=' w-full h-52  bg-slate-100 flex justify-center items-center'>
+                <div  className=' rounded-lg w-60 h-40 bg-gradient-to-r from-pink-600 to-indigo-600  border-2 flex justify-center items-center gap-8 '>
+             {
+              state===false?   <button  className=' rounded-md p-1 bg-gray-200 text-xs ' onClick={()=>audPaly(setState(true))}><FaPlay/></button>:
+              <button  className=' rounded-md p-1 bg-gray-200 text-xs ' onClick={()=>{audioPush(setState(false))}}><FaPause/></button>
+             }
+                 </div>
        </div>
     </>
   )
